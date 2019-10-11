af36bfa8b64e6f46d0fc51b8d1f4044f3986495d
Oleksii Polevin <pololex327@gmail.com>
2019-10-11 13:59:58 +0300

    landing page modified

---
+++ ps1_html-css/landingPage/src/main.js
@@ -0,0 +1,18 @@
+//this variable used for custom correction scrolling position
+const corrector = 50;
+$(document).ready(function() {
+  $(window).scroll(function() {
+		$(document).scrollTop() < corrector ?
+      $("#up").hide() : $("#up").show();
+ });
+ $("#up").click(function() {
+   scrolling(0);
+ })
+})
+function scrolling(x) {
+  window.scrollTo({ top: x, behavior: 'smooth' });
+}
+$("a").click(function() {
+	const x = "#" + $(this).attr("name");
+	scrolling($(x).position().top - corrector);
+})
