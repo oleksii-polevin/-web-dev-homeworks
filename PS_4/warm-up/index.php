cdebe15a3f60b9dae0a0aa1428be95eae807639b
Oleksii Polevin <pololex327@gmail.com>
2019-10-30 13:52:13 +0200

    warm-up completed

--- PS_4/warm-up/index.php
+++ PS_4/warm-up/index.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
@@ -8,11 +10,10 @@
 <body>
 	<div class="count">
 		<?php
-		session_start();
 		isset($_SESSION['counter']) ? $_SESSION['counter'] += 1 : $_SESSION['counter'] = 1;
 		echo "<div><h1>number of visits: $_SESSION[counter]</h1></div>";
-		 ?>
-		 <hr>
+		?>
+		<hr>
 	</div>
 	<div class="first">
 		<h2>The sum of numbers from -1000 to 1000</h2>
@@ -50,10 +51,13 @@
 		</form>
 		<?php
 		if(isset($_SESSION['output_files'])) {
-		echo ($_SESSION['output_files']);
-	}
+			echo ($_SESSION['output_files']);
+		}
 		?>
 		<hr>
+	</div>
+	<div>
+		<h2>chess board</h2>
 	</div>
 	<div class="chessBoard">
 		<form class="" action='upload.php' method="post">
@@ -63,13 +67,13 @@
 		<div>
 			<?php
 			if(isset($_SESSION['chess'])) {
-			echo $_SESSION['chess'];
-		}
-			 ?>
+				echo $_SESSION['chess'];
+			}
+			?>
 		</div>
 	</div>
 	<hr>
-  <div class="sumOf">
+	<div class="sumOf">
 		<h2>Sum of digits from given number</h2>
 		<form class="" action="upload.php" method="post">
 			<input type="text" name="sumOfDigits" placeholder="enter number">
@@ -81,12 +85,14 @@
 			}
 			?>
 		</form>
-		<div class="">
-			<h2>Array</h2>
-			<form class="" action="upload.php" method="get">
-				<button type="submit"name="button">Create Array</button>
-				<p><?php
-				if(isset($_SESSION['array'])) {
+		<hr>
+	</div>
+	<div class="array">
+		<h2>Array</h2>
+		<form class="" action="upload.php" method="get">
+			<button type="submit" name="array">Create Array</button>
+			<p><?php
+			if(isset($_SESSION['array'])) {
 				$array = $_SESSION['array'];
 				foreach ($array as $item => $value) {
 					echo "<div> $item: ";
@@ -94,9 +100,24 @@
 					echo "</div>";
 				}
 			}
-				?></p>
-			</form>
-		</div>
-  </div>
+			?></p>
+		</form>
+		<hr>
+	</div>
+	<div class="textarea">
+		<form action="upload.php" method="post">
+			<h2>Enter some text</h2>
+			<textarea name="textarea" rows="8" cols="80"></textarea>
+			<br>
+			<button type="submit" name="button">Go!</button>
+		</form>
+		<?php
+		if(isset($_SESSION['textarea'])) {
+				foreach ($_SESSION['textarea'] as $key => $value) {
+				echo "<div>$key $value</div>";
+			}
+		}
+		?>
+	</div>
 </body>
 </html>
--- PS_4/warm-up/upload.php
+++ PS_4/warm-up/upload.php
@@ -1,24 +1,31 @@
 <?php
 session_start();
+
+//uploads and downloads files
 if(isset($_FILES["fileToUpload"])) {
   $target_dir = "uploads/";
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
   move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-}
-$names = array_diff(scandir("uploads"), array('..', '.'));
-if(count($names) > 0) {
-  $outputFiles = "";
-  foreach ($names as $item)
-  {
-    $size = human_filesize(filesize("uploads/$item"));
-    $outputFiles .= "<div><a href='/uploads/$item' download>$item</a><span>$size</span></div>";
-    if(previewImg($item)) {
-      $outputFiles .= "<img src='/uploads/$item' style='width: 100px'>";
+
+  $names = array_diff(scandir("uploads"), array('..', '.'));
+  if(count($names) > 0) {
+    $outputFiles = "<section class='files'>";
+    foreach ($names as $item)
+    {
+      $size = humanFilesize(filesize("uploads/$item"));
+      $outputFiles .= "<div><a href='/uploads/$item' download>$item</a><span>$size</span>";
+      if(previewImg($item)) {
+        $outputFiles .= "<img src='/uploads/$item' style='width: 100px'></div>";
+      } else {
+        $outputFiles .="</div>";
+      }
     }
+    $outputFiles .="</section>";
+    $_SESSION['output_files'] = $outputFiles;
   }
-  $_SESSION['output_files'] = $outputFiles;
 }
 
+//creates chess board
 if(isset($_POST['chessBoard'])) {
   $dimensions = $_POST['chessBoard'];
   $arr = preg_split('/\D/', $dimensions);
@@ -29,7 +36,7 @@ if(isset($_POST['chessBoard'])) {
       if($i % 2 === $j % 2) {
         $makeBoard .= "<span style='width: 40px; height: 40px; background-color: black'></span>";
       } else {
-        $makeBoard .= "<span style='width: 40px; height: 40px; background-color: red'></span>";
+        $makeBoard .= "<span style='width: 40px; height: 40px; background-color: white'></span>";
       }
     }
     $makeBoard .="</div>";
@@ -37,6 +44,7 @@ if(isset($_POST['chessBoard'])) {
   $_SESSION['chess'] = $makeBoard;
 }
 
+//sum of digits of given number
 if(isset($_POST['sumOfDigits'])) {
   $numericString = $_POST['sumOfDigits'];
   if(!is_numeric($numericString)) {
@@ -49,9 +57,33 @@ if(isset($_POST['sumOfDigits'])) {
     $_SESSION['sumOfDigits'] = $sum;
   }
 }
+//count number of lines, spaces and symbols
+if(isset($_POST['textarea'])) {
+  $str = $_POST['textarea'];
+  $wrap['text:'] ="<br>" . nl2br($str);
+  $length = mb_strlen($str, 'UTF-8');
+  $newLines = substr_count($str, "\n");
+  $spaces = substr_count($str, " ");
+  $wrap['length:'] = $length - $newLines;
+  //for non-empty strings without \n symbol
+  //also necessary to count last line
+  if($length > 0 && $str[strlen($str) - 1] !== "\n") {
+    $newLines += 1;
+  }
+  $wrap['lines:'] = $newLines;
+  $wrap['spaces:'] = $spaces;
+  $_SESSION['textarea'] = $wrap;
+}
 
-$_SESSION['array'] = createArray();
+//creating arrays
+if(isset($_GET['array'])) {
+  $_SESSION['array'] = createArray();
+}
 
+//return to main page
+header('Location: index.php');
+
+//creates arrays from random numbers
 function createArray()
 {
   //creating multidimensional array for holding results
@@ -73,8 +105,9 @@ function createArray()
   return $wrapper;
 }
 
-header('Location: index.php');
-function human_filesize($bytes, $decimals = 2)
+//returns file size in convinient format
+//borrow this function from php documentation
+function humanFilesize($bytes, $decimals = 2)
 {
   $sz = 'BKMGTP';
   $factor = floor((strlen($bytes) - 1) / 3);
@@ -86,7 +119,3 @@ function previewImg($name)
   $img = '/(.jpg|.png|.bmp|.jpeg|.svg)$/';
   return preg_match($img, $name);
 }
-
-
-
-//
